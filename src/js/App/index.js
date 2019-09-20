d3caf5e6eca2ef0c3bc347dd91b016128349df3b
Nathan Horowitz <nhorowitz@berkeley.edu>
2019-09-20 16:23:52 -0700

    skeleton for create and enter room

--- src/js/App/index.js
+++ src/js/App/index.js
@@ -13,13 +13,20 @@ import firebase from '../Firebase';
 
 import '../../css/index.css';
 
+
+const uuidv1 = require('uuid/v1');
+var pendingRequests = {};
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
           loading: true,
+          localData: {},
         }
         this.renderView = this.renderView.bind(this);
+        this.data = this.data.bind(this);
+        this.updateData = this.updateData.bind(this);
+        this.writeData = this.writeData.bind(this);
     }
 
     componentWillMount() {
@@ -31,6 +38,90 @@ class App extends React.Component {
         }
         this.setState({loading: false});
       });
+    }
+
+    data(tree, id) {
+      if ((tree + id) in pendingRequests) {
+        return;
+      }
+      var current = this.state.localData;
+      if (current[tree] === undefined) {
+        current[tree] = {};
+      }
+      if (current[tree][id] === undefined) {
+        pendingRequests[tree + id] = true;
+        firebase.firestore().collection(tree).doc(id).get().then(doc => {
+          if (!doc.exists) {
+            console.log('No such document!');
+          } else {
+            delete pendingRequests[tree + id];
+            current[tree][id] = doc.data();
+            this.setState({localData: current});
+            return null;
+          }
+        }).catch(err => {
+          console.log('Error getting document', err);
+        });
+      }
+      return current[tree][id];
+    }
+
+    updateData(tree, id, category, value) {
+      var current = this.state.localData;
+      if (current[tree] === undefined) {
+        current[tree] = {};
+      }
+      if (current[tree][id] === undefined) {
+        current[tree][id] = {};
+      }
+      current[tree][id][category] = value;
+      firebase.firestore().collection(tree).doc(id).set({
+        [category]: value
+      }, {merge: true}).then(() => {
+        this.setState({localData: current});
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+
+    writeData(tree, data, id=false) {
+      if (!id) {
+        var write = firebase.firestore().collection(tree).doc()
+        write.set(data).then(() => {
+          var id = write.id
+          var current = this.state.localData
+          if (current[tree] === undefined) {
+            current[tree] = {}
+          }
+          if (current[tree][id] === undefined) {
+            current[tree][id] = {}
+          }
+          for (var key in data) {
+            current[tree][id][key] = data[key]
+          }
+          this.setState({localData: current});
+        }).catch((error) => {
+          console.log("error", error)
+        })
+      } else {
+        var write = firebase.firestore().collection(tree).doc(id)
+        write.set(data).then(() => {
+          var id = write.id
+          var current = this.state.localData
+          if (current[tree] === undefined) {
+            current[tree] = {}
+          }
+          if (current[tree][id] === undefined) {
+            current[tree][id] = {}
+          }
+          for (var key in data) {
+            current[tree][id][key] = data[key]
+          }
+          this.setState({localData: current})
+        }).catch((error) => {
+          console.log("error", error)
+        })
+      }
     }
 
     renderView(name, pathInput=" ") {
@@ -56,6 +147,10 @@ class App extends React.Component {
           return (
               <CreateRoom
                   firebase = {firebase}
+                  uuidv1 = {uuidv1}
+                  data = {this.data}
+                  writeData = {this.writeData}
+                  updateData = {this.updateData}
               ></CreateRoom>
           )
         } else if (name === "Room") {
@@ -70,6 +165,7 @@ class App extends React.Component {
         return (
             <Dashboard
                 firebase = {firebase}
+                data = {this.data}
             ></Dashboard>
         )
       }
@@ -77,6 +173,7 @@ class App extends React.Component {
 
     render() {
       console.log(firebase.auth().currentUser);
+      console.log(this.state.localData);
       if (this.state.loading) {
         return (
           <Loading></Loading>
--- src/js/Dashboard/index.js
+++ src/js/Dashboard/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+
+import Loading from '../Loading';
+
 import { BrowserRouter as Router, Route, Link, Redirect, Switch } from "react-router-dom";
 
 
@@ -11,24 +14,60 @@ class Dashboard extends React.Component {
             redirect: '',
         }
         this.resolveClick = this.resolveClick.bind(this);
+        this.loading = this.loading.bind(this);
+        this.renderRoomIcon = this.renderRoomIcon.bind(this);
+        this.renderRoomIcons = this.renderRoomIcons.bind(this);
     }
 
-    resolveClick(type) {
+    loading() {
+        if (this.props.data('users', this.props.firebase.auth().currentUser.uid)) {
+            const r = this.props.data('users', this.props.firebase.auth().currentUser.uid)['rooms'];
+            for (var i in r) {
+                if (!this.props.data('rooms', r[i])) {
+                    return true;
+                }
+            }
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+    resolveClick(type, id=false) {
         if (type === "CreateRoom") {
             if (true) { //TODO: check token
                 this.setState({redirect: '/createroom'});
             }
         } else if (type === "Room") {
             if (true) { //TODO: check token and id
-                const id = '0xFFFFFFFF'//TODO: set to actual id
                 const pathId = '/room?id=' + id;
                 this.setState({redirect: pathId});
             }
         }
     }
 
+    renderRoomIcon(id) {
+        const d = this.props.data('rooms', id);
+        return (
+            <Button variant="contained" color="primary" onClick={()=>this.resolveClick("Room", id)}>{d['name']}</Button>
+        )
+    }
+
+    renderRoomIcons() {
+        const r = this.props.data('users', this.props.firebase.auth().currentUser.uid)['rooms'];
+        return (
+            <div>
+                {r.map((id) => this.renderRoomIcon(id))}
+            </div>
+        )
+    }
+
     render() {
-        if (this.state.redirect !== '') {
+        if (this.loading()) {
+            return (
+                <Loading></Loading>
+            )
+        } else if (this.state.redirect !== '') {
             return (
                 <Redirect push to={this.state.redirect}></Redirect>
             )
@@ -36,9 +75,7 @@ class Dashboard extends React.Component {
             return (
                 <div>
                     <Typography variant="h1">DASHBOARD</Typography>
-                    <Button variant="contained" color="primary" onClick={()=>this.resolveClick("Room")}>Existing room 1</Button>
-                    <Button variant="contained" color="primary" onClick={()=>this.resolveClick("Room")}>Existing room 2</Button>
-                    <Button variant="contained" color="primary" onClick={()=>this.resolveClick("Room")}>Existing room 3</Button>
+                    {this.renderRoomIcons()}
                     <Button variant="contained" color="secondary" onClick={()=>this.resolveClick("CreateRoom")}>CREATE NEW ROOM</Button>
                 </div>
             )
